abf06a3cee516817f3c250acea7b0288facbee69
rozewskr <rrozewsk@gmail.com>
2020-08-01 13:52:30 -0400

    undef b error

--- src/new.jl
+++ src/new.jl
@@ -187,7 +187,7 @@ function asarray(x::K_, own::Bool=true)
     T, o, s = cinfo(x)
     a = unsafe_wrap(Array, Ptr{T}(x + o), s)
     if own
-        finalizer(K_(pointer(b)-o))
+        finalizer(K_(pointer(a)-o))
     end
     a
 end
