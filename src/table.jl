872e61d4386d1e3cd269eafaccc97fd478041505
rozewskr <rrozewsk@gmail.com>
2020-09-06 12:42:34 -0400

    more new where syntax fix

--- src/table.jl
+++ src/table.jl
@@ -92,7 +92,7 @@ Base.length(iter::K_Table_Iter{T,TS}) where {T,TS} = size(iter.x,1)
 Base.eltype(iter::K_Table_Iter{T,TS}) where {T,TS} = T
 Base._start(iter::K_Table_Iter{T,TS}) where {T,TS} = 1
 
-@generated function Base.next{T,TS}(iter::K_Table_Iter{T,TS}, state)
+@generated function Base.next(iter::K_Table_Iter{T,TS}, state) where {T,TS}
     constructor_call = :($T())
     for i in 1:length(iter.types[2].types)
         push!(constructor_call.args, :(iter.columns[$i][state]))
@@ -100,7 +100,7 @@ Base._start(iter::K_Table_Iter{T,TS}) where {T,TS} = 1
     :($constructor_call, state + 1)
 end
 
-Base.done{T,TS}(iter::K_Table_Iter{T,TS}, state) = state > length(iter)
+Base.done(iter::K_Table_Iter{T,TS}, state) where {T,TS} = state > length(iter)
 
 function K_Table(source)
     iter = TableTraits.getiterator(source)
